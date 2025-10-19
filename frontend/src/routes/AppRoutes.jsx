2385ef7eb4556646b90291ae39112a9798e0467a
rudra <panigrahirudraprasad1@gmail.com>
2025-10-20 04:25:58 +0530

    Added new feature / fixed bug / updated files

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -7,6 +7,9 @@ import FoodPartnerLogin from '../pages/auth/FoodPartnerLogin.jsx';
 import ChooseRegister from '../pages/auth/ChooseRegister.jsx';
 import Home from '../pages/general/Home.jsx';
 import CreateFood from '../pages/food-partner/CreateFoodPartner.jsx';
+import Profile from '../pages/food-partner/Profile.jsx';
+import BottomNav from '../components/BottomNav.jsx';
+import Saved from '../pages/general/Saved.jsx';
 const AppRoutes = () => {
   return (
     <div>
@@ -17,8 +20,10 @@ const AppRoutes = () => {
           <Route path="/food-partner/register" element={<FoodPartnerRegister/>} />
           <Route path="/food-partner/login" element={<FoodPartnerLogin/>} />
           <Route path="/choose-register" element={<ChooseRegister/>} />
-          <Route path="/" element={<Home/>} />
-          <Route path='/create-food' element={<div>Create Food Page - To be implemented</div>} />
+          <Route path="/" element={<><Home/><BottomNav/></>} />
+          <Route path="/saved" element={<><Saved/><BottomNav/></>} />
+          <Route path='/create-food' element={<CreateFood />} />
+          <Route path='/food-partner/profile' element={<Profile/>} />
         </Routes>
       </Router>
     </div>
